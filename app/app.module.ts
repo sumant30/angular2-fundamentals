ef69f7df00f66e7801aa7b9a5b14cd35d1f22477
sumant30 <sumant30@gmail.com>
2017-03-22 21:25:30 +0530

    Added navbar to app

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,9 +3,10 @@ import {BrowserModule }from '@angular/platform-browser';
 import {EventsAppComponent }from './event-app.component'; 
 import {EventListcomponent }from './events/events-list.component'; 
 import {EventThumbnailComponent }from './events/event-thumbnail.component'; 
+import {NavbarComponent} from './nav/navbar.component';
 
 @NgModule( {
-    declarations:[ EventsAppComponent,EventListcomponent,EventThumbnailComponent ], 
+    declarations:[ EventsAppComponent,EventListcomponent,EventThumbnailComponent,NavbarComponent ], 
     imports:[ BrowserModule ], 
     providers:[], 
     bootstrap:[ EventsAppComponent ]
--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -8,7 +8,7 @@ import {Component }from '@angular/core';
     <h1>Upcoming Angular Events</h1>
     </div>
     <hr/>
-    <event-thumbnail [event]="event1"  ></event-thumbnail>
+    <event-thumbnail [event]="event1"></event-thumbnail>
     
     `
 })
