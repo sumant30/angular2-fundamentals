821ceecbafad15a0a1414e4d016e32a2b5d8baba
sumant30 <sumant30@gmail.com>
2017-03-22 21:31:46 +0530

    Chnaged css for navbar

--- app/nav/navbar.component.ts
+++ app/nav/navbar.component.ts
@@ -2,7 +2,29 @@ import {Component }from '@angular/core';
 
 @Component( {
     selector:'nav-bar', 
-    templateUrl:'app/nav/navbar.component.html'
+    templateUrl:'app/nav/navbar.component.html',
+    styles:
+    [
+        `
+        .nav.navbar-nav 
+        {
+            font-size:15px;
+        }
+
+        #searchForm 
+        {
+            margin-right:100px;
+        }
+
+        @media (maxWidth:1200px)
+        {
+            #searchForm 
+            {
+                display:none;
+            }
+        }
+        `
+    ]
 })
 export class NavbarComponent {
     
