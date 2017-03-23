4658c7c9cd06557a867f4ccad3c03ef5eff63a54
sumant30 <sumant30@gmail.com>
2017-03-23 21:06:00 +0530

    Added new page to route in app

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,9 +6,10 @@ import {EventThumbnailComponent }from './events/event-thumbnail.component';
 import {NavbarComponent} from './nav/navbar.component';
 import {EventService}from './events/shared/event.service'; 
 import {ToastrService} from './common/toastr.service';
+import {EventDetailsComponent }from './events/event-details/event-details.component'; 
 
 @NgModule( {
-    declarations:[ EventsAppComponent,EventListcomponent,EventThumbnailComponent,NavbarComponent ], 
+    declarations:[ EventsAppComponent,EventListcomponent,EventThumbnailComponent,NavbarComponent,EventDetailsComponent ], 
     imports:[ BrowserModule ], 
     providers:[EventService,ToastrService], 
     bootstrap:[ EventsAppComponent ]
---
+++ app/events/event-details/event-details.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit }from '@angular/core'; 
+import {EventService}from '../shared/event.service'; 
+
+@Component( {
+    
+    templateUrl:'app/events/event-details/event-details.component.html', 
+    styles:
+    [`
+            .container {
+                padding - left:20px; 
+                padding - right:20px; 
+            }
+
+            .event - image {
+                height:100px; 
+            }
+    `]
+})
+export class EventDetailsComponent implements OnInit {
+    
+    event:any; 
+
+    constructor(private _eventService:EventService) {}
+
+    ngOnInit() {
+        this.event = this._eventService.getEvent(1); 
+    }
+}
