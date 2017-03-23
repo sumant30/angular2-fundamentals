cd2c20747e208b5dcc865fa0a6036f7c2ab5b704
sumant30 <sumant30@gmail.com>
2017-03-23 23:46:48 +0530

    Navigating between pages by using routerLink

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -4,7 +4,7 @@ import { Component,Input,Output,EventEmitter } from '@angular/core';
     selector: 'event-thumbnail',
     template:
     `
-    <div class="well hoverwell thumbnail">
+    <div [routerLink]="['/events',event.id]" class="well hoverwell thumbnail">
     <h2>{{event?.name}}</h2>
     <div>
         Date : {{event?.date}}
