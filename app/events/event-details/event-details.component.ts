fa731e50677907a44b7a557bea1b1774008275ae
sumant30 <sumant30@gmail.com>
2017-04-08 13:49:04 +0530

    routing to the same component

--- app/events/event-details/event-details.component.ts
+++ app/events/event-details/event-details.component.ts
@@ -1,20 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { EventService } from '../shared/event.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ISession } from '../shared/event.model';
+import {
+    Component,
+    OnInit
+} from '@angular/core';
+import {
+    EventService
+} from '../shared/event.service';
+import {
+    ActivatedRoute,
+    Router,
+    Params
+} from '@angular/router';
+import {
+    ISession
+} from '../shared/event.model';
 
 @Component({
 
     templateUrl: 'app/events/event-details/event-details.component.html',
-    styles:
-    [`
+    styles: [`
             .container {
-                padding-left:20px; 
-                padding-right:20px; 
+                padding-left:20px;
+                padding-right:20px;
             }
 
             .event-image {
-                height:100px; 
+                height:100px;
             }
 
             a {cursor:pointer;}
@@ -24,13 +34,17 @@ export class EventDetailsComponent implements OnInit {
 
     event: any;
     addMode: boolean = false;
-    filterBy:string='all';
-    sortBy:string='votes';
+    filterBy: string = 'all';
+    sortBy: string = 'votes';
 
-    constructor(private _eventService: EventService, private _activatedroute: ActivatedRoute, private _router: Router) { }
+    constructor(private _eventService: EventService, private _activatedroute: ActivatedRoute, private _router: Router) {}
 
     ngOnInit() {
-        this.event = this._eventService.getEvent(+ this._activatedroute.snapshot.params["id"]);
+        //this.event = this._eventService.getEvent(+ this._activatedroute.snapshot.params["id"]);
+        this._activatedroute.params.forEach((params:Params) => {
+            this.event = this._eventService.getEvent(+params['id']);
+            this.addMode=false;
+        });
     }
 
     addSession() {
@@ -41,11 +55,11 @@ export class EventDetailsComponent implements OnInit {
         const nextId = Math.max.apply(null, this.event.session(s => s.id));
         session.id = nextId + 1;
         this.event.sessions.push(session);
-        this._eventService.updateEvent(this.event);    
+        this._eventService.updateEvent(this.event);
         this.addMode = false;
     }
 
-    cancelNewSession(){
+    cancelNewSession() {
         this.addMode = false;
     }
 }
